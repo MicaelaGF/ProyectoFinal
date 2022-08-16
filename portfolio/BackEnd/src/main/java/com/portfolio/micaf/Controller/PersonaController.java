d59cddbe07b69325359d383732a7c9da48b5cc4d
MicaelaGF <micaelafoscalino@gmail.com>
2022-08-16 16:37:11 -0300

    cambio estéticos en el frontend

--- portfolio/BackEnd/src/main/java/com/portfolio/micaf/Controller/PersonaController.java
+++ portfolio/BackEnd/src/main/java/com/portfolio/micaf/Controller/PersonaController.java
@@ -30,14 +30,14 @@ public class PersonaController {
        return "La persona fue creada correctamente";
    }
    
-   @DeleteMapping("/personas/borrar/(id)")
+   @DeleteMapping("/personas/borrar/{id}")
    public String deletePersona(@PathVariable Long id) {
        ipersonaService.deletePersona(id);
        return "La persona fue eliminada correctamente";
    }
    
-   //URL:PUERTO/personas7editar/4/nombre & apellido & img
-   @PutMapping("/personas/editar/id")
+   //URL:PUERTO/personas/editar/4/nombre & apellido & img
+   @PutMapping("/personas/editar/{id}")
    public Persona editPersona(@PathVariable Long id,
                               @RequestParam("nombre") String nuevoNombre,
                               @RequestParam("apellido") String nuevoApellido,
