a937c9f25136fd41e4d85a5b11cb491af477e37b
MicaelaGF <micaelafoscalino@gmail.com>
2022-10-25 09:47:21 -0300

    correcciones

--- portfolio/FrontEnd/src/app/service/s-experiencia.service.ts
+++ portfolio/FrontEnd/src/app/service/s-experiencia.service.ts
@@ -8,7 +8,7 @@ import { Experiencia } from '../model/experiencia';
 })
 export class SExperienciaService {
   //expURL = 'https://micafbackend.herokuapp.com/explab/';
-  expURL = 'http://localhost:8080/exlab/';
+  expURL = 'http://localhost:8080/explab/';
 
   constructor(private httpClient: HttpClient) { }
 
